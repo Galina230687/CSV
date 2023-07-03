b95e79ccccb5c4db6429dedc7973115bf95e0267
Galina <serena_05@mail.ru>
2023-07-03 23:19:35 +0300

    second commit

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -7,12 +7,25 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
+
+//import static jdk.jfr.internal.jfc.JFCParser.parseXML;
+
 
 public class Main {
 
@@ -24,6 +37,11 @@ public class Main {
         List<Employee> list = parseCSV(columnMapping, fileName);
         String json = listToJson(list);
         writeString(json, fileNameJson);
+        String fileNameXML = "data.xml";
+        String fileNameJson2 = "data2.json";
+        List<Employee> list2 = parseXML(fileNameXML);
+        String json2 = listToJson(list2);
+        writeString(json2, fileNameJson2);
     }
 
 
@@ -66,5 +84,35 @@ public class Main {
         }
     }
 
+    public static List<Employee> parseXML(String fileNameXML) {
+        List<Employee> list2 = new ArrayList<>();
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(new File("data.xml"));
+            Node root = doc.getDocumentElement();
+            NodeList nodeList = root.getChildNodes();
 
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (node.ELEMENT_NODE == node.getNodeType()) {
+                    Element employee = (Element) node;
+
+                    long id = Long.parseLong(employee.getElementsByTagName("id").item(0).getTextContent());
+                    String firstName = employee.getElementsByTagName("firstName").item(0).getTextContent();
+                    String lastName = employee.getElementsByTagName("lastName").item(0).getTextContent();
+                    String country = employee.getElementsByTagName("country").item(0).getTextContent();
+                    int age = Integer.parseInt(employee.getElementsByTagName("age").item(0).getTextContent());
+                    list2.add(new Employee(id,firstName, lastName, country, age));
+                }
+            }
+
+            return list2;
+
+        } catch (SAXException | IOException | ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
+
